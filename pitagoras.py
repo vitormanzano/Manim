bb3d5f10fc7ec70dc5510e2058a1f333e6532a0c
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2024-08-19 19:26:07 -0300

    Update pitagoras.py

--- pitagoras.py
+++ pitagoras.py
@@ -5,9 +5,10 @@ class triangulo(Scene):
         triangle = Triangle()
         triangle_label = Text('Teorema de pitágoras',font_size = 40).next_to(triangle,2*UP)
         triangle_label.set_fill(YELLOW)
+        
         self.play(FadeIn(Create(triangle),Write(triangle_label)))
         self.wait(2)
-
+        
         square = Square()
         square.set_fill(RED,OPACITY=0.5)
         self.remove(triangle_label)
